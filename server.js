988ff5ba38f94a825dd0b45d1e77409ca0bcc55e
Cody Jennings <cojack91@gmail.com>
2022-11-30 13:52:13 -0500

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.get('/greeting', (req, res) => {
     res.send('Hello, stranger!')
   })
 
+
+//Greeting :name specific
+app.get('/greeting/:name', (req, res) => {
+    res.send('It`s been a month of Sundays since I saw you last ' + req.params.name + ' !')
+  })
+
+
 //Tip Calculator
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     const tip = req.params.total * req.params.tipPercentage
@@ -16,12 +23,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   })
 
 
-//Greeting :name specific
-app.get('/greeting/:name', (req, res) => {
-    res.send('It`s been a month of Sundays since I saw you last ' + req.params.name + ' !')
+//Magic 
+app.get('/magic/:questionasked', (req, res) => {
+    let magicArr = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    const questionAsked = req.params.questionasked
+    let answer = magicArr[Math.floor(Math.random() * magicArr.length)] 
+    res.send(questionAsked + "? " + `<h1>${answer}</h1>`)
   })
-
-
 
 
 app.listen(port, function () {
